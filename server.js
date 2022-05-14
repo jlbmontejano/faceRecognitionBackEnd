d6f0cac24b772ddcbd4e3d18b04e78b4c010486a
unknown <jlbmontejano@gmail.com>
2022-05-14 15:26:39 -0700

    updating database

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-curly-59805",
     user: "postgres",
     password: "password",
     database: "smart-brain",
@@ -22,7 +22,7 @@ const db = knex({
 const app = express();
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send(db.users);
 });
