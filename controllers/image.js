a0e7c841ba041131830a33f1fc14d9801e817d58
unknown <jlbmontejano@gmail.com>
2022-05-15 16:55:32 -0700

    hid API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
-  apiKey: "3ccc8ee741134a17b35ed3a83a1ebb1b",
+  apiKey: process.env.API_CLARIFAI,
 });
 
 const handleApiCall = (req, res) => {
